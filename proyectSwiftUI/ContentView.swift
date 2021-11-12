8c4619e45a27ec7e260e4bd47843e0f491527881
Luiggy102 <luiggytamayo@gmail.com>
2021-11-11 23:09:06 -0500

    Initial Commit

---
+++ proyectSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proyectSwiftUI
+//
+//  Created by Luiggy Esteban Tamayo Erazo on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ proyectSwiftUI/proyectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyectSwiftUIApp.swift
+//  proyectSwiftUI
+//
+//  Created by Luiggy Esteban Tamayo Erazo on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct proyectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
